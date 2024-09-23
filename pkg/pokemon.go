9833e6217639277e6d042c732f13baaf71f879de
RawanMostafa08 <rawanmostafa401@gmail.com>
2024-09-23 13:47:25 +0300

    feat: Logging for pokemon & recsource APIs

--- pkg/pokemon.go
+++ pkg/pokemon.go
@@ -3,19 +3,27 @@ package pokemon
 import (
 	"encoding/json"
 	"fmt"
+
+	"golang.org/x/exp/slog"
 )
 
 func (c Client) Pokemon(id int) (pokemon Pokemon, err error) {
 	c.Endpoint = fmt.Sprintf("/pokemon/%d", id)
 	resp, err := c.sendRequest()
 	if err != nil {
+		slog.Error("error in sending the request: %w", err)
 		return
 	}
 	body, err := readBody(resp)
 	if err != nil {
+		slog.Error("error in reading the response body: %w", err)
 		return
 	}
 	err = json.Unmarshal(body, &pokemon)
-
+	if err != nil {
+		slog.Error("error in pasing the response body: %w", err)
+		return
+	}
+	slog.Info("your pokemon is ready!")
 	return
 }
--- pkg/resource.go
+++ pkg/resource.go
@@ -1,18 +1,28 @@
 package pokemon
 
-import "encoding/json"
+import (
+	"encoding/json"
+
+	"golang.org/x/exp/slog"
+)
 
 func (c Client) Resource(options ...int) (resource Resource, err error) {
 
 	resp, err := c.sendRequest(options...)
 	if err != nil {
+		slog.Error("error in sending the request: %w", err)
 		return
 	}
 	body, err := readBody(resp)
 	if err != nil {
+		slog.Error("error in reading the response body: %w", err)
 		return
 	}
 	err = json.Unmarshal(body, &resource)
-
+	if err != nil {
+		slog.Error("error in pasing the response body: %w", err)
+		return
+	}
+	slog.Info("your resource is ready!")
 	return
 }
