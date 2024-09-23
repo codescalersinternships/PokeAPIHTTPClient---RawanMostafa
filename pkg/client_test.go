f14df26b74c7ec25ff34cc2b3c29d09140921c8a
RawanMostafa08 <rawanmostafa401@gmail.com>
2024-09-23 15:11:07 +0300

    test: Adding testcases for Resource API

--- pkg/client_test.go
+++ pkg/client_test.go
@@ -11,14 +11,29 @@ import (
 func assertEquality(t *testing.T, obj1 any, obj2 any) {
 	t.Helper()
 	if reflect.TypeOf(obj1) != reflect.TypeOf(obj2) {
-		t.Errorf("Error! type mismatch, wanted: %t got: %t", reflect.TypeOf(obj1), reflect.TypeOf(obj2))
+		t.Errorf("Error! type mismatch, wanted: %t \n\n got: %t", reflect.TypeOf(obj1), reflect.TypeOf(obj2))
 	}
 	if !reflect.DeepEqual(obj1, obj2) {
-		t.Errorf("Error! values mismatch, wanted: %v got: %v", obj1, obj2)
+		t.Errorf("Error! values mismatch, wanted: %v \n\n got: %v", obj1, obj2)
 	}
 }
 
-func parseJson(t *testing.T, filePath string) (pokemon Pokemon) {
+func parseResource(t *testing.T, filePath string) (resource Resource) {
+	t.Helper()
+	file, err := os.Open(filePath)
+	if err != nil {
+		t.Errorf("error opening json file %v", err)
+	}
+
+	jsonParser := json.NewDecoder(file)
+	if err = jsonParser.Decode(&resource); err != nil {
+		t.Errorf("error parsing json file %v", err)
+	}
+
+	return
+}
+
+func parsePokemon(t *testing.T, filePath string) (pokemon Pokemon) {
 	t.Helper()
 	file, err := os.Open(filePath)
 	if err != nil {
@@ -34,13 +49,61 @@ func parseJson(t *testing.T, filePath string) (pokemon Pokemon) {
 }
 
 func TestPokemon(t *testing.T) {
-	expectedPokemon:=parseJson(t,"../testdata/pokemon1.json")
+	expectedPokemon := parsePokemon(t, "../testdata/pokemon1.json")
 
-	timeout := time.Second
-	c := NewClient("/pokemon", timeout)
+	c := NewClient("/pokemon", time.Second)
 	gotPokemon, err := c.Pokemon(1)
 	if err != nil {
 		t.Error(err)
 	}
-	assertEquality(t,expectedPokemon,gotPokemon)
+	assertEquality(t, expectedPokemon, gotPokemon)
+}
+
+func TestResource(t *testing.T) {
+
+	testcases := []struct {
+		name             string
+		expectedJsonPath string
+		endpoint         string
+		options          []int
+	}{
+		{
+			name:             "resources of ability, no options",
+			expectedJsonPath: "../testdata/resource_ability.json",
+			endpoint:         "/ability",
+			options:          make([]int, 0),
+		},
+		{
+			name:             "resources of ability, offset=5, limit=5",
+			expectedJsonPath: "../testdata/resource_ability_offset_limit.json",
+			endpoint:         "/ability",
+			options:          []int{5, 5},
+		},
+		{
+			name:             "resources of ability, offset=5",
+			expectedJsonPath: "../testdata/resource_ability_offset.json",
+			endpoint:         "/ability",
+			options:          []int{5},
+		},
+		{
+			name:             "resources of machine, no options",
+			expectedJsonPath: "../testdata/resource_machine.json",
+			endpoint:         "/machine",
+			options:          make([]int, 0),
+		},
+	}
+
+	for _, testcase := range testcases {
+		t.Run(testcase.name, func(t *testing.T) {
+			expectedResource := parseResource(t, testcase.expectedJsonPath)
+
+			c := NewClient(testcase.endpoint, time.Second)
+			gotResource, err := c.Resource(testcase.options...)
+			if err != nil {
+				t.Error(err)
+			}
+			assertEquality(t, expectedResource, gotResource)
+		})
+	}
+
 }
